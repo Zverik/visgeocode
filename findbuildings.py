b934bf3ed43551d4d75d7faf93c7b54fc3adf694
Ilya Zverev <zverik@textual.ru>
2014-12-09 23:21:36 +0300

    proper check for point inside a building, also remove debug code

--- findbuildings.py
+++ findbuildings.py
@@ -2,8 +2,6 @@
 # Script for converting coordinates to enclosing buildings, for visgeocode.html
 import sys, os, cgi, json
 import psycopg2
-import cgitb
-cgitb.enable()
 
 conn = psycopg2.connect('dbname=gis user=zverik')
 cur = conn.cursor()
@@ -18,7 +16,7 @@ ids = set()
 if data and len(data) > 0:
 	for f in data:
 		if 'lat' in f and 'lon' in f:
-			cur.execute('SELECT osm_id, ST_AsGeoJSON(ST_Transform(way, 4326)), ST_Area(way) as area FROM planet_osm_polygon WHERE ST_Transform(ST_SetSRID(ST_Point(%s, %s), 4326), 900913) && way ORDER BY area', (f['lon'], f['lat']))
+			cur.execute('SELECT osm_id, ST_AsGeoJSON(ST_Transform(way, 4326)), ST_Area(way) as area FROM planet_osm_polygon WHERE building is not null AND ST_Contains(way, ST_Transform(ST_SetSRID(ST_Point(%s, %s), 4326), 900913)) ORDER BY area LIMIT 1', (f['lon'], f['lat']))
 			result = cur.fetchone()
 			if result and not result[0] in ids:
 				ids.add(result[0])
